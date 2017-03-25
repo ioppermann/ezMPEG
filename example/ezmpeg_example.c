0be270bb78e1087689c921410b65719217d05524
ioppermann <ingo.oppermann@gmail.com>
2017-03-25 09:06:52 +0100

    Initital import of version 0.1

---
+++ example/ezmpeg_example.c
@@ -0,0 +1,66 @@
+//This file is part of ezMPEG
+//Copyright (C)2002 Ingo Oppermann ( ingo_opp@users.sourceforge.net / http://sourceforge.net/projects/ezmpeg/ )
+//
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either
+//version 2 of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "ezmpeg.h"
+
+int main(int argc, char *argv[])
+{
+	FILE *in;
+	int i, j, m;
+	char *picture;
+	char name[16];
+	clock_t dur;
+	ezMPEGStream ms;
+
+	picture = (char *)malloc(3 * 320 * 240 * sizeof(char));
+
+	if(!ezMPEG_Init(&ms, "ezmpeg_test.mpeg", 320, 240, 25, 30, 2))
+		printf("%s\n", ezMPEG_GetLastError(&ms));
+	if(!ezMPEG_Start(&ms))
+		printf("%s\n", ezMPEG_GetLastError(&ms));
+
+	for(m = 0; m < 75; m++) {
+		strcpy(name, "ezmpeg_test.ppm");
+		if((in = fopen(name, "rb")) == NULL)
+			exit(1);
+
+		fseek(in, 45, SEEK_SET);
+		j = 0;
+		do {
+			i = fgetc(in);
+
+			if(i != EOF)
+				picture[j++] = i;
+			else
+				break;
+		}while(i != EOF);
+
+		fclose(in);
+
+		dur = clock();
+
+		if(!ezMPEG_Add(&ms, picture))
+			printf("%s\n", ezMPEG_GetLastError(&ms));
+
+		dur = clock() - dur;
+		printf("Frame %4d: %f secs\n", m, (float)dur / (float)CLOCKS_PER_SEC);
+	}
+
+	if(!ezMPEG_Finalize(&ms))
+		printf("%s\n", ezMPEG_GetLastError(&ms));
+
+	return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,34 @@
+ezMPEG Copyright (C)2002 Ingo Oppermann (ingo_opp@users.sourceforge.net)
+
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+-----------------------------------------------------------------------------
+
+Welcome to ezMPEG
+
+http://sourceforge.net/projects/ezmpeg
+
+ezMPEG is an easy-to-use and easy-to-understand MPEG1 video encoder API
+
+This documentation is still in development and incomplete. The exact description
+of the encoder API will follow as soon as possible. Until then please have a
+look at the example within this release to see how it works.
+
+To use the API just include 'ezmpeg.h' in your files and compile with
+gcc yourfile.c ezmpeg.c -o yourfile
+
+You are free to use any other C compiler than gcc
