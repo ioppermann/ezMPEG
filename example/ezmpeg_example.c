595d5f8149fe8c00b879e11bb2dd6e5994e84c5f
Ingo Oppermann <ingo.oppermann@gmail.com>
2017-03-26 13:09:16 +0200

    Remove GPL

--- example/ezmpeg_example.c
+++ example/ezmpeg_example.c
@@ -1,15 +1,5 @@
-//This file is part of ezMPEG
-//Copyright (C)2002 Ingo Oppermann ( ingo_opp@users.sourceforge.net / http://sourceforge.net/projects/ezmpeg/ )
-//
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This file is part of ezMPEG
+// Copyright (c) 2002 Ingo Oppermann
 
 #include <stdio.h>
 #include <stdlib.h>
